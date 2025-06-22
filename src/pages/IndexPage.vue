010577c6b9ef605e7194996e4e72329ca35a6025
Kevin Ulrich <71547676+kevin230U@users.noreply.github.com>
2025-06-22 20:40:03 +0200

    Initial commit: frontend Quasar

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,24 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div>
+      <h2>Résultat de l'appel API :</h2>
+      <pre>{{ response }}</pre>
+    </div>
   </q-page>
 </template>
 
 <script setup>
-//
+import { api } from 'boot/axios';
+import { onMounted, ref } from 'vue';
+
+const response = ref('Chargement...')
+
+onMounted(async () => {
+  try {
+    const res = await api.get('api/test')
+    response.value = res.data
+  } catch (err) {
+    response.value = 'Erreur : ' + err.message
+  }
+})
 </script>
